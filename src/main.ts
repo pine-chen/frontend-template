405edd780dc9947a390b82f28e98ee5438d337d5
chen <cs1412223531@gmail.com>
2022-05-06 15:41:30 +0800

    test: test pre-commit lint

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,6 @@ import App from './App.vue';
 import route from './router';
 import 'normalize.css/normalize.css';
 
-createApp(App).use(route).use(createPinia()).mount('#app');
+createApp(App).use(route)
+  .use(createPinia())
+  .mount('#app');
