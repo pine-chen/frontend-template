6697fff8b724620d08837405cd7b08647d4654b7
chen <cs1412223531@gmail.com>
2022-05-10 20:30:58 +0800

    test: test display

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: import('@/features/home/home-template.vue')
+    component: () => import('@/features/home/home-template.vue')
   }
 ];
 
