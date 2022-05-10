faac3d80638745e2bd94b6e86e97ab846e974d18
chen <cs1412223531@gmail.com>
2022-05-10 20:20:34 +0800

    test: test display

--- src/features/home/home-template.vue
+++ src/features/home/home-template.vue
@@ -5,20 +5,32 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
+import { defineComponent } from 'vue';
 import { userStore } from '@/store';
 import { IUser } from '@/features/home/home-template.dto';
 import { user } from '@/service/api';
 
-const store:IUser = userStore();
-const arr1 = [1, 2, 3];
-const arr = arr1.sort((a, b) => a - b).some((item) => item === 2);
-const getList = () => {
-  user.getUser({})
-    .then((res) => {
-      //
-    });
-};
+export default defineComponent({
+  name: 'home-template',
+
+  setup() {
+    const store:IUser = userStore();
+    const arr1 = [1, 2, 3];
+    const arr = arr1.sort((a, b) => a - b).some((item) => item === 2);
+    const getList = () => {
+      user.getUser({})
+        .then((res) => {
+          //
+        });
+    };
+    return {
+      store,
+      arr,
+      getList
+    };
+  }
+});
 </script>
 
 <style lang="scss" scoped>
