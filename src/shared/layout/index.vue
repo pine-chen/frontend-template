42a131a250a35ffafe98a55d07b81652c7fd071e
chen <cs1412223531@gmail.com>
2022-08-04 17:20:01 +0800

    fix: handle menu error

--- src/shared/layout/index.vue
+++ src/shared/layout/index.vue
@@ -1,27 +1,27 @@
 <template>
   <el-container>
     <el-aside width="200px">
-<!--      <el-menu-->
-<!--          background-color="#293750"-->
-<!--          text-color="#fff"-->
-<!--          active-text-color="#FD8701"-->
-<!--          router-->
-<!--      >-->
-<!--        <div class="system-name">-->
-<!--          <img src="@/assets/svgs/system_logo.svg" alt="">-->
-<!--        </div>-->
-<!--        <el-sub-menu v-for="(item, key) in menuData" :key="key" :index="item.path">-->
-<!--          <template #title>-->
-<!--            <span>{{ item.meta.title }}</span>-->
-<!--          </template>-->
-<!--          <el-menu-item-->
-<!--              v-for="(childItem, childKey) in item.children"-->
-<!--              :key="childKey"-->
-<!--              :index="resolvePath(item.path, childItem.path)">-->
-<!--            {{ childItem.meta.title }}-->
-<!--          </el-menu-item>-->
-<!--        </el-sub-menu>-->
-<!--      </el-menu>-->
+      <el-menu
+          background-color="#293750"
+          text-color="#fff"
+          active-text-color="#FD8701"
+          router
+      >
+        <div class="system-name">
+          <img src="@/assets/svgs/system_logo.svg" alt="">
+        </div>
+        <el-sub-menu v-for="(item, key) in menuData" :key="key" :index="item.path">
+          <template #title>
+            <span>{{ item.meta.title }}</span>
+          </template>
+          <el-menu-item
+              v-for="(childItem, childKey) in item.children"
+              :key="childKey"
+              :index="resolvePath(item.path, childItem.path)">
+            {{ childItem.meta.title }}
+          </el-menu-item>
+        </el-sub-menu>
+      </el-menu>
     </el-aside>
     <el-main>
       <router-view></router-view>
@@ -36,7 +36,7 @@ import { IMenuPathFun } from '@/shared/layout/index';
 
 const router = useRouter();
 
-// const menuData = router.options.routes[0].children;
+const menuData = router.options.routes[0].children;
 
 const resolvePath:IMenuPathFun = (basePath:string, routePath:string) => {
   if (isExternal(routePath)) {
